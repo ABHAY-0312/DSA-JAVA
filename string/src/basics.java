4af93d7b45fc2d47c1cef60fab53d2c75f33e8cd
abhay <abhay03december2003@gmail.com>
2024-10-19 11:54:56 +0530

    CHANGE

---
+++ string/src/basics.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class basics {
+    public static void main(String[] args) {
+//        ABHAY
+//        char arr[]={'A','B','H','A','Y'};
+//        for (int i = 0; i <arr.length ; i++) {
+//            System.out.print(arr[i]);
+//        }
+//String x="ABHAY";
+//        System.out.println(x);
+//Take input
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER YOUR NAME");
+        String str=sc.next();
+        System.out.println(str);
+    }
+}
