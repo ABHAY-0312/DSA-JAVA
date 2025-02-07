503d7198ba21debc09e714e31ec52c2c953992ab
Abhay <abhay03december2003@gmail.com>
2025-02-07 19:02:24 +0530

    Your commit message

---
+++ ARRAY/maximusubarraykadan.java
@@ -0,0 +1,20 @@
+public class maximusubarraykadan {
+
+public static void kadanes(int numbers[]){
+    int ms =Integer.MIN_VALUE;
+    int cs =0;
+    for (int i = 0; i < numbers.length; i++) {
+        cs=cs+numbers[i];
+        if(cs<0){
+            cs=0;
+        }
+        ms=Math.max(cs,ms);
+
+    }
+    System.out.println("OUR MAXIMUM SUBARRAY SUM IS :"+ms);
+}
+    public static void main(String[] args) {
+        int numbers[] = {1, -2, 6, -1, 3-2,-3,4,-1,-2,1,5,-3};
+       kadanes(numbers);
+    }
+}
--- ARRAY/subarrayprefix.java
+++ ARRAY/subarrayprefix.java
@@ -1,5 +1,32 @@
 public class subarrayprefix {
-    public static void main(String[] args) {
+    public static void prefixsubarray(int numbers[]) {
+        int currentsum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
         
+        prefix[0] = numbers[0];
+        // Calculate prefix sum
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
+        }
+
+        // Find the maximum subarray sum
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                currentsum = (i == 0) ? prefix[j] : prefix[j] - prefix[i - 1];
+                
+                if (maxSum < currentsum) {
+                    maxSum = currentsum;
+                }
+            }
+        }
+
+        // Print the maximum sum after all comparisons
+        System.out.println("Max sum is : " + maxSum);
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {1, -2, 6, -1, 3};
+        prefixsubarray(numbers);
     }
 }
